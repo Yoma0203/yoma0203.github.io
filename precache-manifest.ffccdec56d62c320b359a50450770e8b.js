6b189e72b3ab1c5f3bd03767e707bfc6ce2dfe01
yoma0203 <agb.yoma@gmail.com>
2020-02-12 21:33:13 -0500

    Updates

---
+++ precache-manifest.ffccdec56d62c320b359a50450770e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58091b94ffa43bdcdfbce48d2ee144e",
+    "url": "/yoma0203.github.io/index.html"
+  },
+  {
+    "revision": "7bfbda7b5f18f5ad2071",
+    "url": "/yoma0203.github.io/static/css/main.41c65691.chunk.css"
+  },
+  {
+    "revision": "eb164aaf477add45fcdc",
+    "url": "/yoma0203.github.io/static/js/2.e4f3ad99.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/yoma0203.github.io/static/js/2.e4f3ad99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfbda7b5f18f5ad2071",
+    "url": "/yoma0203.github.io/static/js/main.1081f979.chunk.js"
+  },
+  {
+    "revision": "9f5a80db9373ce9137fd",
+    "url": "/yoma0203.github.io/static/js/runtime-main.8020cf62.js"
+  }
+]);
